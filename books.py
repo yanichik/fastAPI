10078e8544b32c127b3fb5cdc76ad4f17c5a8b2e
yanichik <brunshte@gmail.com>
2022-05-18 18:30:09 -0700

    required query param "skip_book" of type "string"

--- books.py
+++ books.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 from enum import Enum
 BOOKS = {
     'book_1': {"title": 'title one', "author": 'author one'},
-    'book_2': {"title": 'title one', "author": 'author one'},
-    'book_3': {"title": 'title one', "author": 'author one'},
-    'book_4': {"title": 'title one', "author": 'author one'},
-    'book_5': {"title": 'title one', "author": 'author one'},
+    'book_2': {"title": 'title two', "author": 'author two'},
+    'book_3': {"title": 'title three', "author": 'author three'},
+    'book_4': {"title": 'title four', "author": 'author four'},
+    'book_5': {"title": 'title five', "author": 'author five'},
 }
 class DirectionName(str, Enum):
     north = 'North'
@@ -16,11 +16,19 @@ class DirectionName(str, Enum):
 app = FastAPI()
 
 @app.get("/")
-async def read_all_books():
+async def read_all_books(skip_book: str):
+    if skip_book:
+        new_books = BOOKS.copy()
+        new_books.pop(skip_book)
+        return new_books
     return BOOKS
 
+@app.get("/{book_name}")
+async def read_book(book_name: str):
+    return BOOKS[book_name]
+
 @app.get("/books/mybook")
-async def read_book():
+async def read_my_book():
     return {"book_title": "My Favorite Book"}
 
 @app.get("/books/{book_id}")
