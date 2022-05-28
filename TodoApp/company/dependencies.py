6d4ae176d382a4e9ba30aadceeb21ad7dcdbc38c
yanichik <brunshte@gmail.com>
2022-05-28 14:12:40 -0700

    remove company folder

--- TodoApp/company/dependencies.py
+++
@@ -1,7 +0,0 @@
-import imp
-from fastapi import Header, HTTPException
-
-
-async def get_token_header(internal_token: str = Header(...)):
-    if internal_token != "allowed":
-        raise HTTPException(status_code=400, detail="Internal token header invalid.")
