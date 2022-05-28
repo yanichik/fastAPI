647a586a2f1bd7a7527bd69fa5cd5471a880f5de
yanichik <brunshte@gmail.com>
2022-05-28 14:21:34 -0700

    more cleanup in main

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Depends
 import models
 from database import engine
 from routers import auth, todos, users
-from company import companyapis, dependencies
 
 app = FastAPI()
 
@@ -10,11 +9,4 @@ models.Base.metadata.create_all(bind=engine)
 # this includes all of auth's routes inside the main API
 app.include_router(auth.router)
 app.include_router(todos.router)
-app.include_router(
-    companyapis.router,
-    prefix="/companyapis",
-    tags=["companyapis"],
-    dependencies=[Depends(dependencies.get_token_header)],
-    responses={418: {"description": "Internal Use Only."}},
-)
 app.include_router(users.router)
